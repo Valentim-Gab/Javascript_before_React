ac3631674b02e5b3733d84d884f2fdaf360f4a8f
Valentim_Gab <gabrielvalentimxavier@gmail.com>
2023-10-04 00:10:45 -0300

    Nullish Coalesing Operador

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+//Nullish Coalesing Operador
+
+const idade = 0
+
+document.body.innerHTML = `<h1>Sua idade é: ${idade ?? 'Não informado'}</h1>` 
---
+++ src/nullish-coalesing-operator.ts
@@ -0,0 +1,5 @@
+//Nullish Coalesing Operador
+
+const age = 0
+
+document.body.innerHTML = `<h1>Sua idade é: ${age ?? 'Não informado'}</h1>` 
